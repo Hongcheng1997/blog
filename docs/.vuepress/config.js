3b0ff2fb0429950ac026aaf03d134a6c437acf4d
高洪城 <18367733277@163.com>
2020-04-16 16:08:12 +0800

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     themeConfig: {
         nav: [
             { text: '前端基础', link: '/base/' },
-            { text: '网络相关', link: '/network/' },
+            // { text: '网络相关', link: '/network/' },
             { text: '框架相关', link: '/framework/' },
             { text: 'Github', link: 'https://github.com/Hongcheng1997' },
         ],
